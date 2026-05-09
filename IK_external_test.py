fb99a430560e9c2077a16926ee8e63f9b3914375
Rlshank <r.sandur.vinayaka@student.tue.nl>
2026-05-09 18:21:27 +0200

    improved IK code

--- IK_external_test.py
+++ IK_external_test.py
@@ -1,8 +1,11 @@
-from Inverse_Kinematics import inverse_kinematics as IK
-from Inverse_Kinematics import go_home, is_reachable
+from Inv_Kin import inverse_kinematics as IK
 
 x_des = 0.5
 y_des = 0.5
 
-dtheta_L , dtheta_R = IK(x_des, y_des)
+dtheta_L, dtheta_R = IK(x_des, y_des)
+print(dtheta_L, dtheta_R)
+dtheta_L, dtheta_R = IK(x_des, y_des)
+print(dtheta_L, dtheta_R)
+dtheta_L, dtheta_R = IK(x_des, y_des)
 print(dtheta_L, dtheta_R)
